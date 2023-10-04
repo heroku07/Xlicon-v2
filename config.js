cd62c622493f518e3177c2eea09f5db1e8a6db1e
heroku07 <140170299+heroku07@users.noreply.github.com>
2023-10-04 09:19:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
+global.Owner = ["917907158261"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["917907158261"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["917907158261"];
+global.OwnerName = "Bunny";
+global.BotName = "STEVE-BOT";
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
